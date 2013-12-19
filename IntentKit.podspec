intentkit/IntentKit

commit ab94d1da2d99b557a98a16cd9a090c93347541d6

Allow users to set a default app
Finishes #4

IntentKit.podspec
@@ -10,5 +10,6 @@ Pod::Spec.new do |s|
   s.platform  = :ios, '7.0'
 
   s.source_files = 'IntentKit', 'IntentKit/**/*.{h,m}'
-  s.resource_bundles = { 'IntentKit' => "IntentKit/**/*.{plist,png}" }
+  s.resources = 'IntentKit/en.lproj/*'
+  s.resource_bundles = { 'IntentKit' => "IntentKit/Apps/*.{plist,png}" }
 end

IntentKit/Handlers/INKHandler.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Mike Walker. All rights reserved.
 //
 
+#import "INKActivityViewController.h"
+
 NSString *(^urlEncode)(NSString *);
 
 @class INKActivityPresenter;
@@ -15,7 +17,7 @@ NSString *(^urlEncode)(NSString *);
  ## Subclassing Notes
 
  If you are defining URL schemes for a new discrete class of third-party applications (such as "web browsers" or "mapping applications", you may want to create your own `INKHandler` subclass. All custom methods you create that perform actions should ultimately return a UIActivityController created by calling performCommand:withArguments:. */
-@interface INKHandler : NSObject
+@interface INKHandler : NSObject<INKActivityViewControllerDefaultsDelegate>
 
 /** An array of INKActivity objects that represent all apps, regardless of whether they are available or relevant */
 @property (strong, readonly) NSArray *activities;

IntentKit/Handlers/INKHandler.m
@@ -11,10 +11,12 @@
 #import "INKApplicationList.h"
 #import "INKActivityViewController.h"
 #import "INKActivityPresenter.h"
+#import "INKDefaultsManager.h"
 
 @interface INKHandler ()
 @property (strong, nonatomic) UIApplication *application;
 @property (strong, nonatomic) INKApplicationList *appList;
+@property (strong, nonatomic) INKDefaultsManager *defaultsManager;
 @end
 
 NSString *(^urlEncode)(NSString *) = ^NSString *(NSString *input){
@@ -33,6 +35,7 @@ NSString *(^urlEncode)(NSString *) = ^NSString *(NSString *input){
     if (self = [super init]) {
         self.application = [UIApplication sharedApplication];
         self.appList = [[INKApplicationList alloc] initWithApplication:self.application];
+        self.defaultsManager = [[INKDefaultsManager alloc] init];
     }
 
     return self;
@@ -59,13 +62,43 @@ NSString *(^urlEncode)(NSString *) = ^NSString *(NSString *input){
 
     NSArray *activityItems = @[command, args];
 
-    if (availableApps.count == 1 && !self.alwaysShowActivityView) {
-        INKActivity *app = availableApps.firstObject;
-        [app prepareWithActivityItems:activityItems];
-        return [[INKActivityPresenter alloc] initWithActivity:app];
-    } else {
-        INKActivityViewController *activityView = [[INKActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:[availableApps copy]];
-        return [[INKActivityPresenter alloc] initWithActivitySheet:activityView];
+    if (!self.alwaysShowActivityView) {
+        INKActivity *app;
+
+        if (availableApps.count == 1) {
+            app = availableApps.firstObject;
+        } else if (self.defaultAppName) {
+            NSString *appName = self.defaultAppName;
+            for (INKActivity *theApp in availableApps) {
+                if ([theApp.name isEqualToString:appName]) {
+                    app = theApp;
+                    break;
+                }
+            }
+        }
+
+        if (app) {
+            [app prepareWithActivityItems:activityItems];
+            return [[INKActivityPresenter alloc] initWithActivity:app];
+        }
     }
+
+    INKActivityViewController *activityView = [[INKActivityViewController alloc] initWithActivityItems:activityItems applicationActivities:[availableApps copy]];
+    activityView.delegate = self;
+    return [[INKActivityPresenter alloc] initWithActivitySheet:activityView];
+}
+
+#pragma mark - INKActivityViewControllerDefaultsDelegate methods
+- (void)addDefault:(INKActivity *)activity {
+    [self.defaultsManager addDefault:activity.name forHandler:self.class];
+}
+
+- (BOOL)canSetDefault {
+    return !(BOOL)self.defaultAppName;
+}
+
+#pragma mark - Private methods
+- (NSString *)defaultAppName {
+    return [self.defaultsManager defaultApplicationForHandler:self.class];
 }
 @end

IntentKit/INKActivityViewController/INKActivityPresenter.h
@@ -14,15 +14,15 @@
 /** `INKActivityPresenter` is a presenter object responsible for displaying a `INKActivityViewController`. It will take care of displaying it modally on an iPhone or in a UIPopover on an iPad. */
 @interface INKActivityPresenter : NSObject
 
-/** An `INKActivityViewController` to be displayed. This will be nil iff the user
+/** An `INKActivityViewController` to be displayed. This will be nil if the user
  doesn't need to choose an application (e.g. they only have one installed, or
  have set a preference). */
 @property (strong, nonatomic) INKActivityViewController *activitySheet;
 
-/** An `INKActivity` to be performed iff the user doesn't need to choose an 
- application (e.g. they only have one installed, or have set a preference). */
+/** An `INKActivity` to be performed if the user doesn't need to choose an
+ application (e.g. they only have one installed, or have set a preference). Nil
+ if there is a view controller to be presented instead. */
 @property (strong, nonatomic) INKActivity *activity;
-
 
 /** Instantiate a `INKActivityPresenter` with a given `INKActivityViewController`.
  @param activitySheet The `INKActivityViewController` that will be presentd. */

IntentKit/INKActivityViewController/INKActivityViewController.h
@@ -9,6 +9,21 @@
 #import <UIKit/UIKit.h>
 
 @class INKActivityPresenter;
+@class INKActivity;
+
+/** A delegate object responsible for registering defaults. */
+@protocol INKActivityViewControllerDefaultsDelegate
+/** Register the given activity as a default application.
+ @param activity An INKActivity object representing an application
+ @warning It is assumed the delegate (typically an `INKHandler`) is responsible for maintaining the stateful knowledge of which handler this is for. */
+- (void)addDefault:(INKActivity *)activity;
+
+/** If NO, the toggle view will be disabled/grayed-out. 
+ 
+ This is used when the user's preferred app for a handler cannot perform the task they're trying to do; we don't want them to try to register a secondary default. */
+- (BOOL)canSetDefault;
+@end
+
 
 /** A re-implementation of `UIActivityViewController`, designed to allow for UIActivity objects with custom full-color images.
  
@@ -21,6 +36,9 @@
 /** A presenter object responsible for displaying and hiding the view controller */
 @property (strong, nonatomic) INKActivityPresenter *presenter;
 
+/** A delegate responsible for registering defaults */
+@property (strong, nonatomic) id<INKActivityViewControllerDefaultsDelegate>delegate;
+
 /** The actual content of the action sheet */
 @property (strong, nonatomic) UIView *contentView;
 
@@ -32,8 +50,5 @@
 
  @warning Unlike `UIActivityViewController`, *only* the UIActivity objects specified in the `applicationActivities` array will be displayed; it will not show any of the default activities. */
 - (instancetype)initWithActivityItems:(NSArray *)activityItems applicationActivities:(NSArray *)applicationActivities;
-
-/** Perform the action in the first available application. If no applications are available to perform the action, this will do nothing. */
-- (void)performActivityInFirstAvailableApplication;
 
 @end

IntentKit/INKActivityViewController/INKActivityViewController.m
@@ -11,6 +11,7 @@
 #import "UIView+Helpers.h"
 #import "INKActivityPresenter.h"
 #import "IntentKit.h"
+#import "INKDefaultToggleView.h"
 
 static NSString * const CellIdentifier = @"UIActivityCell";
 
@@ -35,6 +36,7 @@ static CGFloat const INKActivityViewControllerMinimumSpacing_Pad = 10.f;
 @property (strong, nonatomic) UICollectionViewFlowLayout *collectionViewLayout;
 @property (strong, nonatomic) UIButton *cancelButton;
 @property (strong, nonatomic) UIToolbar *blurToolbar;
+@property (strong, nonatomic) INKDefaultToggleView *defaultToggleView;
 
 @property (strong, nonatomic) NSArray *activityItems;
 @property (strong, nonatomic) NSArray *applicationActivities;
@@ -60,37 +62,14 @@ static CGFloat const INKActivityViewControllerMinimumSpacing_Pad = 10.f;
             [self.view addSubview:self.contentView];
         }
 
-        self.collectionViewLayout = [[UICollectionViewFlowLayout alloc] init];
-        self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:self.collectionViewLayout];
-        [self.collectionView registerClass:[INKActivityCell class] forCellWithReuseIdentifier:CellIdentifier];
-        self.collectionView.dataSource = self;
-        self.collectionView.delegate = self;
-
-        [self.contentView addSubview: self.collectionView];
-
-        self.collectionView.backgroundColor = UIColor.clearColor;
-        self.blurToolbar = [[UIToolbar alloc] initWithFrame:self.contentView.bounds];
-        [self.contentView insertSubview:self.blurToolbar atIndex:0];
+        [self addDefaultsToggle];
+        [self addBlurBackground];
 
         if (IntentKit.sharedInstance.isPad) {
-            self.collectionViewLayout.itemSize = INKActivityViewControllerItemSize_Pad;
-            self.collectionViewLayout.sectionInset = INKActivityViewControllerEdgeInsets_Pad;
-            self.collectionViewLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
-
+            [self setUpCollectionViewForPad];
         } else {
-            self.collectionViewLayout.itemSize = INKActivityViewControllerItemSize_Phone;
-            self.collectionViewLayout.sectionInset = INKActivityViewControllerEdgeInsets_Phone;
-            self.collectionView.scrollEnabled = NO;
-            self.collectionViewLayout.minimumInteritemSpacing = INKActivityViewControllerMinimumSpacing_Phone;
-            self.collectionViewLayout.minimumLineSpacing = INKActivityViewControllerMinimumSpacing_Phone;
-
-            self.cancelButton = [UIButton buttonWithType:UIButtonTypeSystem];
-            self.cancelButton.frame = CGRectMake(0, 0, self.view.width, 44.f);
-            [self.cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
-            self.cancelButton.titleLabel.font = [UIFont systemFontOfSize:24.f];
-            self.cancelButton.backgroundColor = UIColor.clearColor;
-            [self.cancelButton addTarget:self action:@selector(didTapCancelButton) forControlEvents:UIControlEventTouchUpInside];
-            [self.contentView addSubview:self.cancelButton];
+            [self setUpCollectionViewForPhone];
+            [self addCancelButton];
         }
 
         [self setBounds];
@@ -102,31 +81,32 @@ static CGFloat const INKActivityViewControllerMinimumSpacing_Pad = 10.f;
     return self.applicationActivities.count;
 }
 
-- (void)performActivityInFirstAvailableApplication {
-    [self.applicationActivities.firstObject prepareWithActivityItems:self.activityItems];
-    [self.applicationActivities.firstObject performActivity];
-}
-
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    self.defaultToggleView.enabled = self.delegate.canSetDefault;
     [self setBounds];
     self.presentingViewController.modalPresentationStyle = UIModalPresentationCurrentContext;
 }
 
 - (void)configureForPad {
+    self.defaultToggleView.frame = CGRectMake(0, 0, self.view.width, self.defaultToggleView.height);
     CGRect frame = CGRectMake(0,
                               0,
                               INKActivityViewControllerWidth_Pad,
-                              INKActivityViewControllerRowHeight_Pad);
+                              INKActivityViewControllerRowHeight_Pad + self.defaultToggleView.height);
     self.view.frame = frame;
     self.blurToolbar.frame = self.contentView.bounds;
     self.collectionView.frame = self.view.bounds;
+    [self.collectionView moveBy:CGPointMake(0, self.defaultToggleView.height)];
+    [self.collectionView resizeTo:CGSizeMake(self.collectionView.width, self.collectionView.height - self.defaultToggleView.height)];
 }
 
 - (void)configureForPhone {
     int numberOfRows = ceil((CGFloat)self.applicationActivities.count / INKActivityViewControllerItemsPerRow_Phone);
 
-    CGFloat viewHeight = self.cancelButton.height + self.collectionViewLayout.sectionInset.bottom + numberOfRows * INKActivityViewControllerRowHeight_Phone;
+    self.defaultToggleView.frame = CGRectMake(0, 0, self.view.width, self.defaultToggleView.height);
+
+    CGFloat viewHeight = self.cancelButton.height + self.defaultToggleView.height + self.collectionViewLayout.sectionInset.bottom + numberOfRows * INKActivityViewControllerRowHeight_Phone;
 
     CGRect frame = CGRectMake(0,
                               self.view.bottom - viewHeight,
@@ -134,6 +114,7 @@ static CGFloat const INKActivityViewControllerMinimumSpacing_Pad = 10.f;
                               viewHeight);
 
     CGRect collectionFrame = self.contentView.bounds;
+    collectionFrame.origin.y += self.defaultToggleView.height;
     collectionFrame.size.height -= self.cancelButton.height;
     self.collectionView.frame = collectionFrame;
 
@@ -173,10 +154,65 @@ static CGFloat const INKActivityViewControllerMinimumSpacing_Pad = 10.f;
 #pragma mark - UICollectionViewDelegate
 
 - (void)collectionView:(UICollectionView *)collectionView didSelectItemAtIndexPath:(NSIndexPath *)indexPath {
-    UIActivity *activity = self.applicationActivities[indexPath.row];
+    INKActivity *activity = self.applicationActivities[indexPath.row];
+
+    if (self.defaultToggleView.isOn) {
+        [self.delegate addDefault:activity];
+    }
+
     [activity prepareWithActivityItems:self.activityItems];
     [activity performActivity];
 
     [self.collectionView deselectItemAtIndexPath:indexPath animated:YES];
 }
+
+#pragma mark - Rendering methods
+- (void)addDefaultsToggle {
+    self.defaultToggleView = [[INKDefaultToggleView alloc] init];
+    [self.contentView addSubview:self.defaultToggleView];
+}
+
+- (void)addBlurBackground {
+    self.blurToolbar = [[UIToolbar alloc] initWithFrame:self.contentView.bounds];
+    [self.contentView insertSubview:self.blurToolbar atIndex:0];
+}
+
+- (void)setUpCollectionViewForPhone {
+    [self setUpCollectionView];
+    self.collectionViewLayout.itemSize = INKActivityViewControllerItemSize_Phone;
+    self.collectionViewLayout.sectionInset = INKActivityViewControllerEdgeInsets_Phone;
+    self.collectionView.scrollEnabled = NO;
+    self.collectionViewLayout.minimumInteritemSpacing = INKActivityViewControllerMinimumSpacing_Phone;
+    self.collectionViewLayout.minimumLineSpacing = INKActivityViewControllerMinimumSpacing_Phone;
+}
+
+- (void)setUpCollectionViewForPad {
+    [self setUpCollectionView];
+    self.collectionViewLayout.itemSize = INKActivityViewControllerItemSize_Pad;
+    self.collectionViewLayout.sectionInset = INKActivityViewControllerEdgeInsets_Pad;
+    self.collectionViewLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+}
+
+- (void)setUpCollectionView {
+    self.collectionViewLayout = [[UICollectionViewFlowLayout alloc] init];
+    self.collectionView = [[UICollectionView alloc] initWithFrame:CGRectZero collectionViewLayout:self.collectionViewLayout];
+    [self.collectionView registerClass:[INKActivityCell class] forCellWithReuseIdentifier:CellIdentifier];
+    self.collectionView.dataSource = self;
+    self.collectionView.delegate = self;
+
+    [self.contentView addSubview: self.collectionView];
+
+    self.collectionView.backgroundColor = UIColor.clearColor;
+}
+
+- (void)addCancelButton {
+    self.cancelButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.cancelButton.frame = CGRectMake(0, 0, self.view.width, 44.f);
+    [self.cancelButton setTitle:@"Cancel" forState:UIControlStateNormal];
+    self.cancelButton.titleLabel.font = [UIFont systemFontOfSize:24.f];
+    self.cancelButton.backgroundColor = UIColor.clearColor;
+    [self.cancelButton addTarget:self action:@selector(didTapCancelButton) forControlEvents:UIControlEventTouchUpInside];
+    [self.contentView addSubview:self.cancelButton];
+}
+
 @end

IntentKit/INKDefaultToggleView.h
@@ -1,0 +1,17 @@
+//
+//  INKDefaultToggleView.h
+//  IntentKitDemo
+//
+//  Created by Michael Walker on 12/18/13.
+//  Copyright (c) 2013 Mike Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/** A view responsible for showing a 'set as default' toggle switch */
+@interface INKDefaultToggleView : UIControl
+
+/** Whether or not the choice should be saved as a default. */
+@property (nonatomic, assign) BOOL isOn;
+
+@end

IntentKit/INKDefaultToggleView.m
@@ -1,0 +1,93 @@
+//
+//  INKDefaultToggleView.m
+//  IntentKitDemo
+//
+//  Created by Michael Walker on 12/18/13.
+//  Copyright (c) 2013 Mike Walker. All rights reserved.
+//
+
+#import "INKDefaultToggleView.h"
+#import "UIView+Helpers.h"
+
+@interface INKDefaultToggleView ()
+
+@property (strong, nonatomic) UISwitch *toggle;
+@property (strong, nonatomic) UILabel *label;
+@property (strong, nonatomic) UIView *bottomLine;
+@property (strong, nonatomic) UIView *disabledShade;
+@end
+
+@implementation INKDefaultToggleView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.toggle = [[UISwitch alloc] init];
+        [self addSubview:self.toggle];
+
+        self.label = [[UILabel alloc] init];
+        self.label.text = NSLocalizedStringFromTable(@"SetDefaultToggleLabel", @"IntentKit-Localizable", nil);
+        self.label.textColor = [UIColor colorWithRed:0 green:0 blue:0 alpha:0.7];
+        UITapGestureRecognizer *tapRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(didTapTextLabel)];
+        [self.label addGestureRecognizer:tapRecognizer];
+        self.label.userInteractionEnabled = YES;
+        [self addSubview:self.label];
+
+        self.bottomLine = [[UIView alloc] init];
+        self.bottomLine.backgroundColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.2];
+        [self addSubview:self.bottomLine];
+
+        [self resizeTo:CGSizeMake(0, 50.f)];
+    }
+    return self;
+}
+
+- (BOOL)isOn {
+    return self.toggle.isOn;
+}
+
+- (void)setIsOn:(BOOL)isOn {
+    [self.toggle setOn:isOn animated:YES];
+}
+
+- (void)setEnabled:(BOOL)enabled {
+    [super setEnabled:enabled];
+    self.alpha = (enabled ? 1.0 : 0.4);
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    self.bottomLine.frame = CGRectMake(0, self.bottom-1, self.width, 1);
+    self.disabledShade.frame = self.bounds;
+}
+
+- (void)updateConstraints {
+    [super updateConstraints];
+
+    NSArray *layoutStrings = @[@"|-15-[toggle]-10-[label]-15-|",
+                               @"V:|-10-[toggle]-10-|",
+                               @"V:|-10-[label]-10-|"
+                               ];
+
+    NSDictionary *views = @{@"toggle": self.toggle,
+                            @"label": self.label};
+    for (NSString *layoutString in layoutStrings) {
+        [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:layoutString
+                                                                     options:0
+                                                                     metrics:nil
+                                                                       views:views]];
+    }
+
+    for (NSString *key in views) {
+        [views[key] setTranslatesAutoresizingMaskIntoConstraints:NO];
+    }
+}
+
+#pragma mark - Event handlers
+- (void)didTapTextLabel {
+    [self.toggle setOn:!self.toggle.isOn animated:YES];
+}
+
+
+@end

commit b00ddacda0c9b134612c167180c3a172e07ebf45

Fix podspec regression caused by ab94d1d

IntentKit.podspec
@@ -10,6 +10,5 @@ Pod::Spec.new do |s|
   s.platform  = :ios, '7.0'
 
   s.source_files = 'IntentKit', 'IntentKit/**/*.{h,m}'
-  s.resources = 'IntentKit/en.lproj/*'
-  s.resource_bundles = { 'IntentKit' => "IntentKit/Apps/*.{plist,png}" }
+  s.resource_bundles = { 'IntentKit' => "IntentKit/**/*.{plist,png}" }
 end