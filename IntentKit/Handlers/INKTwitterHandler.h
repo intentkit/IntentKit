0e2293fd036ba19a5ba25cf3aaae9292f1ed2fed
Jeff Blagdon <jeffblagdon@gmail.com>
2014-03-10 16:30:59 -0400

    Twitter connect / mentions issue resolved, removed warning in docstring

--- IntentKit/Handlers/INKTwitterHandler.h
+++ IntentKit/Handlers/INKTwitterHandler.h
@@ -43,9 +43,8 @@
 
 
 /** Shows @mentions for the active user
- @return A `INKActivityPresenter` object to present.
+ @return A `INKActivityPresenter` object to present. */
 
- @warning The first-party Twitter app does not have a "mentions" screen any more. If this results in opening Twitter.app, the "Connect" page will be displayed, which aggregates @mentions, DMs, and favorites/retweets/etc. */
 - (INKActivityPresenter *)showMentions;
 
 /** Shows DMs for the active user
